c1697b9d2d404c2234384183488d79c01a394b5d
Andy <andrew.parrish@nhs.net>
2015-02-03 08:05:57 -0800

    improved test file

--- test1.py
+++ test1.py
@@ -6,18 +6,24 @@ from LrgParser import LrgParser
 
 class TestFixture(unittest.TestCase):
 
-	def setUp(self):
-		self.test_lrg = LrgParser('LRG_TEST.xml', 10)
+    def setUp(self):
+        self.test_lrg = LrgParser('LRG_TEST.xml', 10)
 
-	def test_LrgParser(self):
-		#test_lrg = LrgParser('LRG_TEST.xml', 10)
-		self.assertEqual(str(self.test_lrg.__class__), 'LrgParser.LrgParser')
+    def test_LrgParser(self):
+        self.assertEqual(str(self.test_lrg.__class__), 'LrgParser.LrgParser')
 
-	def test_grab_element(self):
-		element = self.test_lrg.grab_element('fixed_annotation/sequences')
-		self.assertEqual(str(element.__class__), "<type 'str'>")
-		self.assertEqual(element[0:60], 'GAAAAGAAAAGAAAAGAAAAGAAAAGAAAAGAAAAGAAAAGAAAAGAAAAGAAAAGAAAA')
+    def test_grab_element(self):
+        """This test should pass based on the LRG_TEST.xml file 3/2/2015"""
+        element = self.test_lrg.grab_element('fixed_annotation/sequence')
+        self.assertEqual(str(element.__class__), "<type 'str'>")
+        self.assertEqual(element[0:60], 'GAAAAGAAAAGAAAAGAAAAGAAAAGAAAAGAAAAGAAAAGAAAAGAAAAGAAAAGAAAA')
+    
+    def test_grab_element_fail(self):
+        """This test should fail, as it references a non-existent node"""
+		element = self.test_lrg.grab_element('fixed_annotation/failure')
+		self.assertEqual(str(element.__class__),  "<type 'NoneType'>")
+		self.assertEqual(element, None)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
