0cceab1e9b35222740e4c9dd3ea8a79ba6ee5c32
Matt Welland <mattwellie@gmail.com>
2015-03-26 10:32:27 +0000

    Update LrgParser.py

--- LrgParser.py
+++ LrgParser.py
@@ -50,8 +50,6 @@ class LrgParser:
                 'updatable_annotation/annotation_set/lrg_locus').text
             self.transcriptdict['refseqname'] = self.transcriptdict['root'].find(
                 'fixed_annotation/sequence_source').text
-            self.transcriptdict['filename'] = self.transcriptdict['genename'] + '_' + self.fileName.split('/')[-1].split('.')[
-                0] + '_' + str(padding)
 
             if self.transcriptdict['root'].attrib['schema_version'] != '1.9':
                 print 'This LRG file is not the correct version for this script'
